988379cdda412628bb8dc182a544826f829fe304
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-03-02 12:44:03 +0100

    Update index.ts

--- formulas/count-attachments/index.ts
+++ formulas/count-attachments/index.ts
@@ -1,4 +1,4 @@
 export const meta = import.meta;
-export const formulaSourceCodePath = import.meta.resolve(`./index.formula`);
+export const formulaSourceCodePath = import.meta.resolve(`./formula`);
 // const formulaSource = await fetch(`${meta.url}/${index.formula}`).then(r => r.text());
 export metaJson from './meta.json';
