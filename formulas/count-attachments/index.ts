d9bc9f6182c3e4a58fa17514035dc88cd441f1bf
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-03-02 17:48:37 +0100

    Update index.ts

--- formulas/count-attachments/index.ts
+++ formulas/count-attachments/index.ts
@@ -1,9 +1,9 @@
 export const meta = import.meta;
 const templateUrl = new URL(import.meta.url, `./formula`);
-export const tUrl = templateUrl.toString();
-export {templateUrl}
+const tUrl = templateUrl.toString();
+export {templateUrl, tUrl}
 
-export const templateText = await fetch(templateUrl).then(r => r.text());
+// export const templateText = await fetch(templateUrl).then(r => r.text());
 
 // const formulaSource = await fetch(`${meta.url}/${index.formula}`).then(r => r.text());
 export metaJson from './meta.json';
