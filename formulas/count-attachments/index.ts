487e3eb78e778343b91fbee3c66f2534f9f9ad2a
Dominik Bošnjak <47727604+dddominikk@users.noreply.github.com>
2026-03-02 12:33:58 +0100

    Update index.ts

--- formulas/count-attachments/index.ts
+++ formulas/count-attachments/index.ts
@@ -1,5 +1,5 @@
 export const meta = import.meta;
-import * as FORMULA from 'index.formula';
+import * as FORMULA from './index.formula';
 import metaJson from './meta.json';
 export {FORMULA, metaJson};
 export default {FORMULA, meta, metaJson};
